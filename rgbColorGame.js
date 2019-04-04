82722625b00a54e850aa993b99c8d4b4acf97db5
Crescendo Account 1 <crescendoaccount1@Emilys-MacBook-Pro-2.local>
2019-04-04 12:41:16 -0500

    added in functionality so that squares that do not match the current color of the body's background will fade out of view.

--- rgbColorGame.js
+++ rgbColorGame.js
@@ -1,12 +1,29 @@
+var bodyBG = window.getComputedStyle(document.getElementsByTagName("body")[0])
+    .backgroundColor;
 var squares = document.getElementsByClassName("square");
 var randomColors = [];
 var pickedColor;
 var pickedColorDisplay = document.getElementById("pickedColorDisplay");
 
+setSquareEventListeners(squares);
 randomColors = generateRandomColors();
 pickedColor = selectRandomColor(randomColors);
 pickedColorDisplay.textContent = pickedColor;
 
+function setSquareEventListeners(squares) {
+    for (i = 0; i < squares.length; i++) {
+        squares[i].addEventListener("click", function() {
+            if (this.style.backgroundColor === bodyBG) {
+            } else {
+                fadeOutSquare(this);
+            }
+        });
+    }
+}
+
+function fadeOutSquare(square) {
+    square.style.opacity = 0;
+}
 function generateRandomColors() {
     var colors = [];
     for (i = 0; i < squares.length; i++) {
