b2dc6a0f18250837e4297b83ff077275b91574dc
Crescendo Account 1 <crescendoaccount1@Emilys-MacBook-Pro-2.local>
2019-04-05 08:10:00 -0500

    Refined animations and sequences for correct guesses made. Added player feedback span to show either Try Again or Correct depending on the selected square matching the picked color. Reorganized functions in javascript file for alphabetical order.

--- rgbColorGame.js
+++ rgbColorGame.js
@@ -5,38 +5,43 @@ var randomColors = [];
 var pickedColor;
 var pickedColorDisplay = document.getElementById("pickedColorDisplay");
 var isColorPicked = false;
+var messageDisplay = document.querySelector("#message");
+var h1Display = document.getElementsByTagName("h1")[0];
 
 setSquareEventListeners(squares);
 setup();
 
-function setup() {
-    randomColors = generateRandomColors();
-    pickedColor = selectRandomColor(randomColors);
-    pickedColorDisplay.textContent = pickedColor;
-}
-
-function setSquareEventListeners(squares) {
-    for (i = 0; i < squares.length; i++) {
-        squares[i].addEventListener("click", function() {
-            checkGuess(this);
-        });
-    }
-}
-function fadeInSquare(square) {
-    square.style.opacity = 1;
-}
-function fadeOutSquare(square) {
-    square.style.opacity = 0;
-}
 function checkGuess(square) {
     if (square.style.backgroundColor === pickedColor) {
+        correctGuess();
+    } else {
+        incorrectGuess(square);
+    }
+}
+function correctGuess() {
+    matchColors();
+    for (j = 0; j < squares.length; j++) {
+        fadeInSquare(squares[j]);
+    }
+
+    messageDisplay.textContent = "Correct!";
+
+    window.setTimeout(function() {
         for (i = 0; i < squares.length; i++) {
             fadeOutSquare(squares[i]);
         }
-        window.setTimeout(setup, 200);
-    } else {
-        fadeOutSquare(square);
-    }
+        resetHeaderBGColor();
+        messageDisplay.innerHTML = "&nbsp";
+        window.setTimeout(setup, 400);
+    }, 2000);
+}
+function fadeInSquare(square) {
+    square.style.opacity = 1;
+    square.style.cursor = "pointer";
+}
+function fadeOutSquare(square) {
+    square.style.opacity = 0;
+    square.style.cursor = "initial";
 }
 function generateRandomColors() {
     var colors = [];
@@ -48,16 +53,40 @@ function generateRandomColors() {
     }
     return colors;
 }
-
-function selectRandomColor(randomColors) {
-    var x = Math.floor(Math.random() * Math.floor(6));
-    return randomColors[x];
+function incorrectGuess(square) {
+    fadeOutSquare(square);
+    messageDisplay.textContent = "Try Again!";
 }
-
+function matchColors() {
+    for (i = 0; i < squares.length; i++) {
+        squares[i].style.backgroundColor = pickedColor;
+    }
+    h1Display.style.backgroundColor = pickedColor;
+}
 function randomBGColor() {
     var x = Math.floor(Math.random() * 256);
     var y = Math.floor(Math.random() * 256);
     var z = Math.floor(Math.random() * 256);
     var bgColor = "rgb(" + x + ", " + y + ", " + z + ")";
     return bgColor;
+}
+
+function resetHeaderBGColor() {
+    h1Display.style.backgroundColor = bodyBG;
+}
+function selectRandomColor(randomColors) {
+    var x = Math.floor(Math.random() * Math.floor(6));
+    return randomColors[x];
+}
+function setSquareEventListeners(squares) {
+    for (i = 0; i < squares.length; i++) {
+        squares[i].addEventListener("click", function() {
+            checkGuess(this);
+        });
+    }
+}
+function setup() {
+    randomColors = generateRandomColors();
+    pickedColor = selectRandomColor(randomColors);
+    pickedColorDisplay.textContent = pickedColor;
 }
